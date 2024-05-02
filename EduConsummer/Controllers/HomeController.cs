360348622fa28f1fd3b348a1ef2d730c4a54d7d9
kendor74 <kendor1222002@gmail.com>
2024-05-02 21:51:16 +0300

    putting Tutor pages

--- EduConsummer/Controllers/HomeController.cs
+++ EduConsummer/Controllers/HomeController.cs
@@ -43,7 +43,29 @@
             return RedirectToAction("LiveStreamRoom");
         }
         
-        
+
+
+
+        public IActionResult Tutor()
+        {
+            List<Tutor> dataList = new List<Tutor>
+        {
+            new Tutor { Name = "Mohamed Abdulaziz",  Descrp  = "Mohamed is an experienced math tutor with a passion for teaching algebra, calculus, and geometry.",
+                Field = "Mathematics",           ImageSrc = "Male-tutor-1440x960.jpeg" },
+            new Tutor { Name = "Mohamed El-Hoseny", Descrp  = "Mohamed specializes in physics tutoring, covering topics such as mechanics, thermodynamics, and electromagnetism.",
+                Field = "Physics",     ImageSrc = "201710_students_Johannsen_Shane_Tutoring_02.jpg" },
+            //new Tutor { Name = "Orange", Descrp  = "Yet another fruit", Field = "Fruit", ImageSrc = "orange.jpg" }
+            // Add more items as needed
+        };
+
+
+            return View(dataList);
+        }
+
+        public IActionResult TutorPageIndividual()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
@@ -52,4 +74,6 @@
 
 
     }
+
+    
 }
--- EduConsummer/Models/Users/Student.cs
+++ EduConsummer/Models/Users/Student.cs
@@ -4,4 +4,12 @@
     {
 
     }
+    class Tutor
+    {
+        public string Name { get; set; }
+        public string Descrp { get; set; }
+        public string Field { get; set; }
+        public string ImageSrc { get; set; }
+
+    }
 }
